8c66ce5079801e64202217106adadf3b75b97923
fairuzajs <yaubasarova.93@mail.ru>
2024-03-02 20:21:58 +0500

    feat: add 1

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,10 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  const date1 = new Date(date);
+  const date2 = new Date(0);
+  return date1 - date2;
 }
 
 /**
@@ -31,7 +33,7 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
+function getTime(date) {
   throw new Error('Not implemented');
 }
 
